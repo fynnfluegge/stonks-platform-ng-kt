ff6763511966f33092c035c724fe4bd732c314c4
fynnfluegge <fynnfluegge@gmx.de>
2021-01-30 18:01:27 +0100

    web node.js gzip express-static-gzip working

--- web/server.js
+++ web/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const compression = require('compression')
-var expressStaticGzip = require("express-static-gzip");
+const expressStaticGzip = require("express-static-gzip")
 const app = express()
 
-app.use("/", expressStaticGzip("var/www/"));
+app.use('/*', expressStaticGzip('dist/var/www/', {
+    enableBrotli: true,
+    customCompressions: [{
+        encodingName: 'deflate',
+        fileExtension: 'zz'
+    }],
+    orderPreference: ['br']
+}));
 
 app.all('/*', function(req, res, next) {
-    res.sendFile('index.html.gz', { root: 'var/www' });
+    res.sendFile('index.html.gz', { root: 'dist/var/www' });
 });
 
 app.listen(3000, () => {console.log('Server listen on port 3000')})
