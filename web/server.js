a1ef9bc0f938d6f234d648c72f3c0d830c8370ee
fynnfluegge <fynnfluegge@gmx.de>
2021-01-30 14:03:26 +0100

    index.html.gz

--- web/server.js
+++ web/server.js
@@ -6,7 +6,7 @@ app.use(express.static('var/www'));
 app.use(compression());
 
 app.all('/*', function(req, res, next) {
-    res.sendFile('index.html', { root: 'var/www' });
+    res.sendFile('index.html.gz', { root: 'var/www' });
 });
 
 app.listen(3000, () => {console.log('Server listen on port 3000')})
