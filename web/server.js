a75715a15dcb2be54745dad9823e4a2b102fac28
fynnfluegge <fynnfluegge@gmx.de>
2021-01-31 14:01:31 +0100

    node.js stuff

--- web/server.js
+++ web/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const compression = require('compression');
+const path = require('path');
 const expressStaticGzip = require("express-static-gzip");
 const app = express();
 
 app.use(compression());
 
-app.use('/*', express.static(__dirname + '/build/var/www'));
+app.use(express.static(path.join(__dirname, '/build/var/www'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/build/var/www/index.html'));
+    res.sendFile(path.join(__dirname, '/build/var/www/index.html'));
 })
 
 app.listen(3000, () => {console.log('Server listen on port 3000')})
