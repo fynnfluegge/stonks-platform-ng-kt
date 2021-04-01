24ce0a24494bf5dedada75355fe5621d31b2cba5
fynnfluegge <fynnfluegge@gmx.de>
2021-04-01 14:21:56 +0200

    ng build production environment.ts

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  enableDebug: false,
+  production: false,
+  enableDebug: true,
   apiUrl: 'http://localhost:9091'
 };
 
